de1cae185f2fcfd72abda5692155ab9420127487
ckbhatia <chetansain86@gmail.com>
2020-01-13 18:17:17 +0530

    Updates

---
+++ precache-manifest.925cdb658c5b62e84937f2f434c579db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa99b1450d3e05b464cd33db22be44a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8ce6b2c1b9ff5d49d32f",
+    "url": "/portfolio/static/css/2.247797c6.chunk.css"
+  },
+  {
+    "revision": "765262d5057d43b37fb3",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8ce6b2c1b9ff5d49d32f",
+    "url": "/portfolio/static/js/2.e544d096.chunk.js"
+  },
+  {
+    "revision": "765262d5057d43b37fb3",
+    "url": "/portfolio/static/js/main.6a1d45c2.chunk.js"
+  },
+  {
+    "revision": "5f864619c4faecbce6ea",
+    "url": "/portfolio/static/js/runtime-main.b36185b3.js"
+  }
+]);
